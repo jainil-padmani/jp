d2c8fecae166fac4ad239b75b617febcef42af98
jainil-padmani <58204742+jainil-padmani@users.noreply.github.com>
2025-01-01 20:30:42 +0530

    Update Hero.tsx
    mailto live ph

--- apps/web/app/(landing)/home/Hero.tsx
+++ apps/web/app/(landing)/home/Hero.tsx
@@ -6,30 +6,37 @@ import { env } from "@/env";
 import { HeroAB } from "@/app/(landing)/home/HeroAB";
 import HeroVideoDialog from "@/components/HeroVideoDialog";
 
-export function HeroText(props: {
+export function HeroText({
+  children,
+  className,
+}: {
   children: React.ReactNode;
   className?: string;
 }) {
-  const { className, ...rest } = props;
-
   return (
     <h1
       className={cn("font-cal text-4xl text-gray-900 sm:text-6xl", className)}
-      {...rest}
-    />
+    >
+      {children}
+    </h1>
   );
 }
 
-export function HeroSubtitle(props: { children: React.ReactNode }) {
-  return <p className="mt-6 text-lg leading-8 text-gray-600" {...props} />;
+export function HeroSubtitle({ children }: { children: React.ReactNode }) {
+  return <p className="mt-6 text-lg leading-8 text-gray-600">{children}</p>;
 }
 
 export function HeroHome() {
-  if (env.NEXT_PUBLIC_POSTHOG_HERO_AB) return <HeroAB />;
+  if (env.NEXT_PUBLIC_POSTHOG_HERO_AB)
+    return <HeroAB variantKey={env.NEXT_PUBLIC_POSTHOG_HERO_AB} />;
   return <Hero />;
 }
 
-export function Hero(props: {
+export function Hero({
+  title,
+  subtitle,
+  image,
+}: {
   title?: React.ReactNode;
   subtitle?: React.ReactNode;
   image?: string;
@@ -39,30 +46,27 @@ export function Hero(props: {
       <SquaresPattern />
       <div className="pt-24 sm:pb-12 sm:pt-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          {/* <HeroTag /> */}
           <div className="mb-10">
             <ProductHuntBadge />
           </div>
 
           <div className="mx-auto max-w-xl text-center">
             <HeroText>
-              {props.title || "Stop wasting half your day in Gmail"}
+              {title || "Stop wasting half your day in Gmail"}
             </HeroText>
             <HeroSubtitle>
-              {props.subtitle ||
-                "Automate your email with AI, bulk unsubscribe from newsletters, and block cold emails. Open-source."}
+              {subtitle ||
+                "Automate your email with AI, bulk unsubscribe from newsletters, and block cold emails."}
             </HeroSubtitle>
             <CTAButtons />
           </div>
-
-          <LogoCloud />
 
           <div className="relative mt-16 flow-root sm:mt-24">
             <HeroVideoDialog
               className="block"
               animationStyle="top-in-bottom-out"
-              videoSrc="https://www.youtube.com/embed/hfvKvTHBjG0?autoplay=1"
-              thumbnailSrc={props.image || "/images/newsletters.png"}
+              videoSrc="https://ftaxatshahchanel.com"
+              thumbnailSrc={image || "/images/newsletters.png"}
               thumbnailAlt="Bulk Unsubscriber Screenshot"
             />
           </div>
@@ -72,56 +76,22 @@ export function Hero(props: {
   );
 }
 
-// function HeroTag() {
-//   return (
-//     <div className="mb-8 flex justify-center bg-white">
-//       <div className="relative flex items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-//         <a
-//           href="/product-hunt"
-//           className="flex items-center gap-x-1 font-semibold text-blue-600"
-//         >
-//           <span className="absolute inset-0" aria-hidden="true" />
-//           We are live on Product Hunt!
-//           <ChevronRightIcon
-//             className="-mr-2 h-5 w-5 text-gray-400"
-//             aria-hidden="true"
-//           />
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
-
 function ProductHuntBadge() {
   return (
     <div className="flex flex-col items-center justify-center gap-2 sm:flex-row">
       <a
-        href="https://www.producthunt.com/posts/inbox-zero-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-inbox&#0045;zero&#0045;2"
+        href="https://www.producthunt.com/posts/mailto-live?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-mailto&#0045;live"
         target="_blank"
         rel="noreferrer"
       >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=431438&theme=neutral&period=daily"
-          alt="Inbox&#0032;Zero - Clean&#0032;up&#0032;your&#0032;inbox&#0032;in&#0032;minutes&#0044;&#0032;open&#0032;source | Product Hunt"
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=727877&theme=light"
+          alt="Mailto Live - AI Email Management & Gmail Cleanup Tool | Product Hunt"
           className="h-[54px] w-[250px]"
           width="250"
           height="54"
         />
       </a>
-
-      {/* <a
-        href="https://www.producthunt.com/posts/inbox-zero-5?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-inbox&#0045;zero&#0045;2"
-        target="_blank"
-      >
-        <img
-          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=431438&theme=light"
-          alt="Inbox&#0032;Zero - Clean&#0032;up&#0032;your&#0032;inbox&#0032;in&#0032;minutes&#0044;&#0032;open&#0032;source | Product Hunt"
-          className="h-[54px] w-[250px]"
-          width="250"
-          height="54"
-        />
-      </a> */}
     </div>
   );
 }
